094fa99e22d7be8d2c512eaa54bc68d65c1c0425
linkah <developer@linkah.com.br>
2026-01-27 14:18:11 -0300

    push

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,6 +1,6 @@
 const { Resend } = require('resend');
 
 // Usamos process.env para segurança, mas a chave que você mandou deve estar no Render
-const resend = new Resend(process.env.RESEND_API_KEY || 're_gHQdcehD_JtWrrfTzh3LpCHMTLNX1Y4Mo');
+const resend = new Resend(process.env.RESEND_API_KEY || 're_HwHuMfRb_6J4UzpkVZPXoupyeRRsnigmR');
 
 module.exports = resend;
