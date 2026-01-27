15b46203b6d80e854d674fa42c7065838e0aa363
linkah <developer@linkah.com.br>
2026-01-27 14:25:50 -0300

    Sistema de senha oficial ativado

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,6 +1,6 @@
 const { Resend } = require('resend');
 
-// Usamos process.env para segurança, mas a chave que você mandou deve estar no Render
-const resend = new Resend(process.env.RESEND_API_KEY || 're_HwHuMfRb_6J4UzpkVZPXoupyeRRsnigmR');
+// Use a nova chave que você gerou
+const resend = new Resend('re_HwHuMfRb_6J4UzpkVZPXoupyeRRsnigmR');
 
 module.exports = resend;
