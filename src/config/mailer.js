427dc9178c15d0912b6990d648f1dc9e202b3f1b
linkah <developer@linkah.com.br>
2026-01-27 13:01:08 -0300

    mails

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,15 +1,14 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // Usar o atalho 'service' ajuda o Nodemailer a configurar tudo sozinho
+  service: 'gmail', // Usar 'service' é mais seguro que configurar host/porta manualmente
   auth: {
-    user: process.env.EMAIL_USER,
+    user: process.env.EMAIL_USER, // Agora vai ler seu e-mail correto do Render
     pass: process.env.EMAIL_PASS,
   },
-  // Se ainda der erro, mantenha estas opções de timeout:
-  connectionTimeout: 10000, // 10 segundos
-  greetingTimeout: 10000,
-  socketTimeout: 10000
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = transporter;
