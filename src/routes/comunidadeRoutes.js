e321fa1ce7c6f1fffbccf0b7f43f0d71c21c6aab
linkah <developer@linkah.com.br>
2026-02-04 11:28:06 -0300

    alt

--- src/routes/comunidadeRoutes.js
+++ src/routes/comunidadeRoutes.js
@@ -2,20 +2,15 @@ const express = require('express');
 const router = express.Router();
 const comunidadeController = require('../controllers/comunidadeController');
 
-// Lista as mensagens (O parâmetro :evento_id deve ser o mesmo que o controller espera)
+// 1. Lista as mensagens de um evento específico
+// (O parâmetro :evento_id deve ser o mesmo que o controller espera)
 router.get('/:evento_id', comunidadeController.listarMensagensPorEvento);
 
-// Envia a mensagem (Corrigido de 'salvarMensagem' para 'enviarMensagem')
+// 2. Envia a mensagem (Corrigido de 'salvarMensagem' para 'enviarMensagem')
 router.post('/enviar', comunidadeController.enviarMensagem);
 
-module.exports = router;const express = require('express');
-const router = express.Router();
-const comunidadeController = require('../controllers/comunidadeController');
-
-router.get('/:evento_id', comunidadeController.listarMensagensPorEvento);
-router.post('/enviar', comunidadeController.enviarMensagem);
-
-// NOVA ROTA PARA USUÁRIOS ONLINE
+// 3. NOVA ROTA PARA USUÁRIOS ONLINE
+// (Utiliza o id do evento para verificar/atualizar quem está online)
 router.get('/:id/online', comunidadeController.atualizarPresenca);
 
 module.exports = router;
