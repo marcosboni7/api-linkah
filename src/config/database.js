c217563048d2455238f5d8b84086cb4236559334
linkahdev <linkahdev@proton.me>
2026-07-25 11:45:35 -0300

    corrige conexao postgres ssl neon

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 
-// Remove parâmetros extras da URL que podem causar conflito no driver pg do Node
 let connectionString = process.env.DATABASE_URL;
 if (connectionString) {
   connectionString = connectionString.split('&channel_binding=')[0];
@@ -11,7 +10,6 @@ const isProduction = !!connectionString;
 const pool = new Pool({
   connectionString: connectionString || undefined,
   ssl: isProduction ? { rejectUnauthorized: false } : false,
-  // Fallback caso use as variáveis separadas
   ...(!connectionString && {
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
@@ -21,13 +19,12 @@ const pool = new Pool({
   })
 });
 
-// Esse log TEM que aparecer no Render
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error('❌ ERRO NA CONEXÃO COM O BANCO:', err.message);
   } else {
     const hostInfo = process.env.DATABASE_URL ? process.env.DATABASE_URL.split('@')[1].split('/')[0] : process.env.DB_HOST;
-    console.log('🐘 CONECTADO AO BANCO:', hostInfo); // Loga o host sem a senha
+    console.log('🐘 CONECTADO AO BANCO:', hostInfo);
   }
 });
 
